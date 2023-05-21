843445ab4f2cee6ee729ee0d7b1fb683e7809c1e
Diogo <72268280+diogorrio@users.noreply.github.com>
2023-05-21 16:40:41 +0200

    Harmonizing the entire project

--- fm_finding/fm_utils/__init__.py
+++ fm_finding/fm_utils/__init__.py
@@ -1,27 +1,34 @@
-import getpass
-
-from fm_finding.fm_finding import FMFinding
+from fm_finding.fm_finding import FMFinding, password
 from graph_data.db_structure import create_db, create_table
 
 
 def main():
     # Load database
-    password = getpass.getpass("Please insert the server password: ")
     create_db('forbidden_minors', 'localhost', 'root', password)
+    create_table('fm_in_f1', 'forbidden_minors', 'localhost', 'root', password)
+    create_table('fm_in_f2', 'forbidden_minors', 'localhost', 'root', password)
+    create_table('fm_in_f3', 'forbidden_minors', 'localhost', 'root', password)
     create_table('fm_in_f4', 'forbidden_minors', 'localhost', 'root', password)
     create_table('fm_in_f5', 'forbidden_minors', 'localhost', 'root', password)
 
-    # TODO: Load graph data
+    # TODO: Load graph data (if using generated graphs from 'nauty', specifically geng)
 
     # Find forbidden minors through various techniques; create object and use different methods
-    # For f(4)
-    fm_f4 = FMFinding("f4")
-    fm_f4.tree_decompose()
-    # For f(5)
-    fm_f5 = FMFinding("f5")
-    fm_f5.tree_decompose()
+    # Testing, for F(1), F(2) or F(3)
+    fm_f3 = FMFinding("F(3)")
+    fm_f3.combinatorial_enumeration(5)
 
-    # Check for exhaustiveness
+    # For F(4)
+    fm_f4 = FMFinding("F(4)")
+    # For 8 (or more) vertices, it takes too long at the moment
+    # fm_f4.combinatorial_enumeration(7)
+    # fm_f4.random_sampling(10, 0.6)
+
+    # For F(5)
+    # fm_f5 = FMFinding("F(5)")
+    # fm_f5.tree_decompose()
+
+    # TODO: Check for exhaustiveness
 
 
 main()
